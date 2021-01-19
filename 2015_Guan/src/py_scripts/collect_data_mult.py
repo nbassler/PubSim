fa02198544752a1826c20aa82f609a92052aa48e
fkalholm <fredrik.kalholm@fysik.su.se>
2021-01-19 16:18:55 +0100

    finished sims

--- 2015_Guan/src/py_scripts/collect_data_mult.py
+++ 2015_Guan/src/py_scripts/collect_data_mult.py
@@ -2,9 +2,9 @@ import os
 import json
 
 rel_path = '../../data/output/'
-thickn_char = 76
-row_in_detect = 21
-header = 'PMMA AvgEnergyPrim DLET DLETPrim DLETProt TLET TLETPrim TLETProt dZ2Beta2 dZ2Beta2Prim dZ2Beta2Prot dZeff2Beta2 dZeff2Beta2Prim dZeff2Beta2Prot tZeff2Beta2 tZeff2Beta2Prim tZeff2Beta2Prot'
+#thickn_char = 76
+row_in_detect = 27
+header = 'PMMA AvgEnergyPrim Dose DosePrim DoseProt DLET DLETPrim DLETProt TLET TLETPrim TLETProt dQ dQPrim dQProt tQ tQPrim tQProt dZeff2Beta2 dZeff2Beta2Prim dZeff2Beta2Prot tZeff2Beta2 tZeff2Beta2Prim tZeff2Beta2Prot'
 dirname = os.path.dirname(__file__)
 data_path = os.path.join(dirname, rel_path + 'wdir/')
 
@@ -14,14 +14,24 @@ subfolders = [x[0] for x in os.walk(data_path)][1:]
 
 # Openfing txt file where the data shall be collected
 with open(data_path + '../collected_data.txt', 'w') as col:
+
+    # writing header data
     col.write(header + '\n')
+
     # going through subfolders, first writing the LET value as reference (taken from the folder name), then adding the data in the SH-file
     for i in subfolders:
         _, _, filenames = next(os.walk(i))
+
+        # collects if the filename starts with 'data'
         for j in filenames:
             if j[0:4] == 'data':
                 with open(i + '/' + j, 'r') as f:
-                    col.write(i[len(data_path):])
-                    col.write(f.readlines()[row_in_detect])
-                    f.close()
+                    try:
+                        a = f.readlines()[row_in_detect]
+                        col.write(i[len(data_path):])
+                        col.write(a)
+                        f.close()
+                    except:
+                        print('Failed on file: ' + i + '/' + j, 'r')
+
     col.close()
--- 2015_Guan/src/py_scripts/plot_data.py
+++ 2015_Guan/src/py_scripts/plot_data.py
@@ -1,28 +1,28 @@
 import os
 import numpy as np
 import pandas as pd
+import math
 import matplotlib.pyplot as plt
 
 rel_path = '../../data/output/'
 dirname = os.path.dirname(__file__)
 data_path = os.path.join(dirname, rel_path + 'wdir/')
 
+rows = 5
+cols = 5
+
 # loading data into pandas
 with open(data_path + '../collected_data.txt', 'r') as col:
     df = pd.read_csv(col, delimiter=' ')
 
-
 thick = df.PMMA.unique()
-print(thick)
 headers = list(df.columns.values)
 
 df_mean = pd.DataFrame(columns=headers)
-df_std = pd.DataFrame(columns=headers)
-print(df_std)
+df_stdom = pd.DataFrame(columns=headers)
 
 
-# dictionary with all mean and std values, with each thickness as a key, and [mean, std] as value
-vals = {}
+# creating two new dataframes:One with mean values and one with stds
 for i in thick:
     filt = df['PMMA'] == i
     df_i = df[filt]
@@ -30,25 +30,35 @@ for i in thick:
     # mean value
     df_mean.loc[str(i)] = df_i.mean()
 
-    # std
-    df_std.loc[str(i)] = df_i.std()
+    # stdom, dividing std with sqrt of number of data-files
+    df_stdom.loc[str(i)] = df_i.std().div(math.sqrt(len(df_i.index)))
+
+    if i == '4.2':
+        print(df_i)
+        print('mean:')
+        print(df_i.mean())
+        print('stdom:')
+        print(df_i.std().div(math.sqrt(len(df_i.index))))
+
+print(df_mean)
+print(df_stdom)
+print(thick)
 
 r1 = 0
 r2 = 0
-fig, axs = plt.subplots(4, 4)
-for i in headers[1:]:
+fig, axs = plt.subplots(rows, cols)
+for i in headers[1:rows*cols:]:
     x = thick
     y = df_mean[i]
-    yerr = df_std[i]
+    yerr = df_stdom[i]
 
-    axs[r1, r2].errorbar(x, y, yerr=yerr)
+    axs[r1, r2].errorbar(x, y, yerr=yerr, fmt='bo')
     axs[r1, r2].set_title(i)
-    #axs[r1, r2].set_xlabel('PMMA thickness')
+    # axs[r1, r2].set_xlabel('PMMA thickness')
 
     # changing axis position
     r1 += 1
-    if r1 > 3:
+    if r1 > rows - 1:
         r1 = 0
         r2 += 1
-
 plt.show()
