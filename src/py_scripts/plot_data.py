37aa6ff97910f45eab41d16e837616ab157d0dcf
fkalholm <fredrik.kalholm@fysik.su.se>
2021-05-14 16:03:41 +0200

    added z>0 filter for Guan

--- src/py_scripts/plot_data.py
+++ src/py_scripts/plot_data.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 rel_path = '../../data/output/'
 dirname = os.path.dirname(__file__)
 data_path = os.path.join(dirname, rel_path + 'wdir/')
+file_path = '/home/fredo/projects/PubSim/2015_Guan/output/collected_data_2.2e7_parts_truncated.txt'
 
 rows = 5
 cols = 5
 
 # loading data into pandas
-with open(data_path + '../collected_data_chaudhary2014_stragg1.txt', 'r') as col:
-    df = pd.read_csv(col, delimiter=' ')
+with open(file_path, 'r') as col:
+    df = pd.read_csv(col, delimiter=',')
 
+df['1_Over_Avg_Energy'] = np.divide(1, df['AvgEnergyPrim'])
 thick = df.PMMA.unique()
 headers = list(df.columns.values)
 
@@ -51,4 +53,45 @@ for i in headers[1:rows*cols:]:
     if r1 > rows - 1:
         r1 = 0
         r2 += 1
+plt.show()
+
+rows = 5
+cols = 4
+# Plotting vs DLET_prim on x-axix
+df_mean.sort_values(by=['DLETPrim'], inplace=True)
+print(df_mean['DLETPrim'])
+r1 = 0
+r2 = 0
+fig, axs = plt.subplots(rows, cols)
+for i in headers[1:]:
+
+    if i[1] == 'o':
+        print(i)
+        continue
+    if i[0] == 'A':
+        print(i)
+        continue
+
+    x = df_mean['DLETPrim']
+    y = df_mean[i]
+    yerr = df_stdom[i]
+
+    if i != 'DLETPrim':
+        axs[r1, r2].errorbar(x, y, yerr=yerr, fmt='bo', label=i)
+        axs[r1, r2].plot(x, y)
+        #axs[r1, r2].set_title(i)
+    else:
+        axs[r1, r2].errorbar(x, y, yerr=yerr, fmt='ro', label=i)
+        axs[r1, r2].plot(x, y, 'r-')
+    axs[r1, r2].legend()
+    # axs[r1, r2].set_xlabel('PMMA thickness')
+
+    # changing axis position
+    r1 += 1
+    if r1 > rows - 1:
+        r1 = 0
+        r2 += 1
+fontsize = 20
+fig.text(0.5, 0.04, 'LETd_Primary', ha='center', fontsize=fontsize)
+fig.text(0.04, 0.5, 'Quantity', va='center', rotation='vertical', fontsize=fontsize)
 plt.show()
