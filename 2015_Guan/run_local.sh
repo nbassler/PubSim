ea257c3b5d09db674e64dd370432168ea1db0e7b
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-06-29 19:19:04 +0200

    cleaning

--- 2015_Guan/run_local.sh
+++ 2015_Guan/run_local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) snakemake --config nprim=100  stop_time=00:01:00 -j24
+TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) snakemake --config nprim=1000 stop_time=00:01:00 pmma_samples=2 -j4
--- 2015_Guan/run_local_plgrid.sh
+++ 2015_Guan/run_local_plgrid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 module load plgrid/tools/python/3.9
-TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) PATH=$PATH:$SCRATCH/shieldhit snakemake --config nprim=1000 stop_time=00:01:00 -j24
+TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) PATH=$PATH:$SCRATCH/shieldhit snakemake --config nprim=1000 stop_time=00:01:00 pmma_samples=2 -j24
