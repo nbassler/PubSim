28877dac8fea26a0f5ae3698d2f85ec52be3e59f
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-12-10 11:06:13 +0100

    run script improved

--- 2015_Guan/run_local_plgrid.sh
+++ 2015_Guan/run_local_plgrid.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 module load plgrid/tools/python/3.9
-TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) PATH=$PATH:$SCRATCH/shieldhit snakemake --config nprim=10000 stop_time=00:01:00 pmma_samples=2 -j24
+source ./venv/bin/activate
+INPUT_FILTER="H[W|T]*" TIMEPATTERN=$( date +%Y_%m_%d_%H_%M_%S ) PATH=$PATH:$SCRATCH/shieldhit snakemake --config nprim=10000 stop_time=00:01:00 pmma_samples=2 -j24
